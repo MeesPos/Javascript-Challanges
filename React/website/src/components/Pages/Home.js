b3b652c540f5ae37ab7c5153b48cdd4123e04279
MeesPos <meespostma_@hotmail.com>
2021-01-13 18:24:26 +0100

    CSS and Javascript variables

---
+++ React/website/src/components/Pages/Home.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import Header from '../Common/Header';
+import image from '../assets/img/header-bg.jpg';
+
+// Re-usable components
+import Services from '../Common/Services';
+import Portfolio from '../Common/Portfolio';
+
+class Home extends Component {
+
+    render() {
+        return (
+            <div>
+                <Header
+                    title="Welcome to our Studio!"
+                    subtitle="IT'S NICE TO MEET YOU"
+                    buttonText="Tell me more"
+                    Link="/services"
+                    showButton={true}
+                    image={image}
+                />
+
+                <Services />
+                <Portfolio />
+            </div>
+        )
+    }
+}
+
+export default Home;
